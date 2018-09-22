fa6d9409f6a1c079eb0e82048dc8b95b6a956231
Vincent <icwei9@gmail.com>
2018-09-22 14:31:38 +1000

    addPost functionality

--- resolvers.js
+++ resolvers.js
@@ -3,16 +3,30 @@ module.exports = {
     getUser: () => null
   },
   Mutation: {
+    addPost: async (
+      _,
+      { title, imageURL, categories, description, creatorId },
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        imageURL,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
+    },
     signUpUser: async (_, { username, email, password }, { User }, info) => {
       // I think this can be shorted with a .then, etc
       const user = await User.findOne({ username });
       if (user) {
-        throw new Error('User already exists');
+        throw new Error("User already exists");
       }
 
       const newUser = await new User({
-        username, 
-        email, 
+        username,
+        email,
         password
       }).save();
       return newUser;
