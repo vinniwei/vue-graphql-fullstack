9ae182a19f6fa0c05987591146974dad9d462a08
Unknown <icwei9@gmail.com>
2018-09-26 21:28:51 +1000

    theme uploaded

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,25 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import colors from "vuetify/es5/util/colors";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.teal.accent3,
+    secondary: colors.teal.accent1,
+    accent: colors.teal.accent4,
+    error: colors.red.accent2,
+    warning: colors.yellow.lighten2,
+    info: colors.blue.base,
+    success: colors.lime.accent4
+  }
+});
 
 new Vue({
   router,
